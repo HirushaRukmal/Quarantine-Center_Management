bda33fc5d0eda124f2e068918a127bb606c0ef43
Isuru Pathum Herath <isurupathumherath@gmail.com>
2021-08-22 12:46:05 +0530

    test merge

--- server/server.js
+++ server/server.js
@@ -36,3 +36,4 @@ app.use('/salary', employeeSalaryRoute); //--Added by Isuru Pathum Herath--
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
+//Isuru Pathum Herath
